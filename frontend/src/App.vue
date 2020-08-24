ae6263402f2f4c84e080ec31d268ba9359d99e60
Alisher Agzamov <alisher.agzamov@gmail.com>
2020-08-25 02:04:44 +0500

    Routes refactoring

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,18 +13,8 @@
   export default {
     name: 'app',
     components: {
-      'Navigation': Navigation,
+      Navigation,
       Breadcrumbs
     }
   }
 </script>
-
-<style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -15,99 +15,27 @@ import EditTask from '@/components/EditTask'
 import CreateTask from '@/components/CreateTask'
 import ShowUser from '@/components/ShowUser'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Index',
-      component: Index
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/signup',
-      name: 'Signup',
-      component: Signup
-    },
-    {
-      path: '/dashboard',
-      name: 'Dashboard',
-      component: Dashboard
-    },
-    {
-      path: '/tasks',
-      name: 'Tasks',
-      component: Tasks
-    },
-    {
-      path: '/tasks/create',
-      name: 'CreateTask',
-      component: CreateTask
-    },
-    {
-      path: '/tasks/:id',
-      name: 'ShowTask',
-      component: ShowTask
-    },
-    {
-      path: '/tasks/edit/:id',
-      name: 'EditTask',
-      component: EditTask
-    },
-    {
-      path: '/logout',
-      name: 'Logout',
-      component: Logout
-    },
-    {
-      path: '/profile',
-      name: 'Profile',
-      component: Profile
-    },
-    {
-      path: '/users',
-      name: 'Users',
-      component: Users
-    },
-    {
-      path: '/users/create',
-      name: 'CreateUser',
-      component: CreateUser
-    },
-    {
-      path: '/users/edit/:id',
-      name: 'EditUser',
-      component: EditUser
-    },
-    {
-      path: '/users/:id/tasks',
-      name: 'UserTasks',
-      component: Tasks
-    },
-    {
-      path: '/users/:id/tasks/create',
-      name: 'UserTasksCreate',
-      component: CreateTask
-    },
-    {
-      path: '/users/:user_id/tasks/:id',
-      name: 'ShowUserTask',
-      component: ShowTask
-    },
-    {
-      path: '/users/:user_id/tasks/:id/edit',
-      name: 'EditUserTask',
-      component: EditTask
-    },
-    {
-      path: '/users/:id',
-      name: 'ShowUser',
-      component: ShowUser
-    }
+    { path: '/', name: 'Index', component: Index },
+    { path: '/login', name: 'Login', component: Login },
+    { path: '/signup', name: 'Signup', component: Signup },
+    { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+    { path: '/tasks', name: 'Tasks', component: Tasks },
+    { path: '/tasks/create', name: 'CreateTask', component: CreateTask },
+    { path: '/tasks/:id', name: 'ShowTask', component: ShowTask },
+    { path: '/tasks/edit/:id', name: 'EditTask', component: EditTask },
+    { path: '/logout', name: 'Logout', component: Logout },
+    { path: '/profile', name: 'Profile', component: Profile },
+    { path: '/users', name: 'Users', component: Users },
+    { path: '/users/create', name: 'CreateUser', component: CreateUser },
+    { path: '/users/edit/:id', name: 'EditUser', component: EditUser },
+    { path: '/users/:id/tasks', name: 'UserTasks', component: Tasks },
+    { path: '/users/:id/tasks/create', name: 'UserTasksCreate', component: CreateTask },
+    { path: '/users/:user_id/tasks/:id', name: 'ShowUserTask', component: ShowTask },
+    { path: '/users/:user_id/tasks/:id/edit', name: 'EditUserTask', component: EditTask },
+    { path: '/users/:id', name: 'ShowUser', component: ShowUser }
   ]
 })
